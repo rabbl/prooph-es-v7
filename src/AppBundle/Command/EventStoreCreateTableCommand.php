60a580931e3d575ead303e5abde18900cc35d7d7
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-02-26 22:43:18 +0100

    Add createEventStreamTable command with hardcoded params

---
+++ src/AppBundle/Command/EventStoreCreateTableCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Prooph\EventStore\StreamName;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class EventStoreCreateTableCommand extends ContainerAwareCommand
+{
+    protected function configure(): void
+    {
+        // Name and description for app/console command
+        $this
+            ->setName('prooph:eventstore:create')
+            ->setDescription('Creates the eventstore table.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $connection = $this->getContainer()->get('event_store_pdo');
+        $strategy = $this->getContainer()->get('prooph_event_store.mysql.aggregate_stream_strategy');
+        $schema = $strategy->createSchema($strategy->generateTableName(new StreamName('event_stream_test')));
+        foreach ($schema as $command) {
+            $statement = $connection->prepare($command);
+            $statement->execute();
+        }
+    }
+}
